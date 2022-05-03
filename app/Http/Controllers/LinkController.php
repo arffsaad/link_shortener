c51747e3e33e1a077e6dad132dbfac98e06da189
arffsaad <80538339+arffsaad@users.noreply.github.com>
2022-05-03 12:11:03 +0800

    upload

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\Models\Link;
+use Illuminate\Http\RedirectResponse;
+
+class LinkController extends Controller
+{
+    public function landing(){
+        return view('welcome');
+    }
+
+    public function createLink(Request $request){
+        $data = $request->all();
+        $random = Str::random(8);
+        if (substr($data['target'],4) != "http"){
+            $data['target'] = "http://" . $data['target'];
+        }
+        Link::create([
+            'target' => $data['target'],
+            'identifier' =>  $random
+        ]);
+        $url = config('app.url') . "/red/" . $random;
+        return view('welcome', compact('url'));
+    }
+
+    public function redirect(){
+        $ident = request()->route('ident');
+        $redir = Link::where('identifier', $ident)->pluck('target');
+        return redirect()->away($redir[0]);
+    }
+}
